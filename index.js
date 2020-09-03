e25b92362bbc4746a45d88cb420d0bab19d4d077
Aditya <pratapsinghaditya2001@gmail.com>
2020-09-03 09:38:26 +0530

    Bomb hits witch detection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let witch = document.querySelector('.witch');
 let baloons = document.querySelectorAll('.baloon');
 let playArea = document.querySelector('body');
+let witch_life = 100;
 
 // Responsible for bombs that are thrown from 
 // left side of each baloon
@@ -45,6 +46,39 @@ setInterval(function(){
   })
 },62.5)
 
+// For moving fire from bottom to top
+setInterval(function(){
+  let fires = document.querySelectorAll('.fire');
+  fires.forEach(fire => {
+    let initialFireTop = fire.getBoundingClientRect().top;
+    fire.style.top = initialFireTop - 5.75 + 'px';
+  })
+},62.5)
+
+// For detecting if bomb hits witch
+setInterval(function(){
+  let bombs = document.querySelectorAll('.bomb');
+  bombs.forEach(bomb => {
+    let bomb_top = bomb.getBoundingClientRect().top;
+    let bomb_left = bomb.getBoundingClientRect().left;
+    let witch_left = witch.getBoundingClientRect().left;
+    if((bomb_top >= (0.72*screen.height)) && (bomb_top <= screen.height)){
+      if(bomb_left <= witch_left){
+        let diff = witch_left - bomb_left;
+        if(diff<=0.01*screen.width){
+          alert('Collision!');
+          witch_life = witch_life - 33.33;
+        }
+      }
+      else if(bomb_left <= witch_left + witch.offsetWidth - (0.02*screen.width)){
+        alert('Collision!');
+        witch_life = witch_life - 33.33;
+      }
+      bomb.style.display = 'none';
+    } 
+  })
+},10)
+
 // For moving witch towards left
 function moveLeft(){
   let initialPosLeft = witch.getBoundingClientRect().left;
@@ -60,6 +94,20 @@ function moveRight(){
   if(initialPosRight < max_right){
     witch.style.left = initialPosRight + 20 + 'px';
   }
+}
+
+// For fire from witch's end
+function fire(){
+  let fire = document.createElement('div');
+  let image = document.createElement('img');
+  image.src = './img/fire2.png';
+  fire.appendChild(image);
+  fire.classList.add('fire');
+  fire.style.position = 'absolute';
+  fire.style.top =  witch.getBoundingClientRect().top + 'px';
+  // bomb.style.left = 0.15*screen.width + 'px';
+  fire.style.left = witch.getBoundingClientRect().left + 'px';
+  playArea.appendChild(fire);   
 }
 
 window.addEventListener('keydown',(e)=>{
